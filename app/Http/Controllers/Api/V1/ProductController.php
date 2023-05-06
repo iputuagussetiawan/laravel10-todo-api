5454179522508514a412701248b3b86f224c37cf
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-06 18:00:38 +0800

    Product Delete

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -51,6 +51,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response()->noContent();
     }
 }
